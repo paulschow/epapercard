ef636b5ccf491bb8d5a62aa8f1183f473ee28501
Paul Schow <paulschow@gmail.com>
2016-07-06 11:10:16 -0600

    Added comments

--- firmware/paul_demo_200_isr/paul_demo_200_isr.ino
+++ firmware/paul_demo_200_isr/paul_demo_200_isr.ino
@@ -13,30 +13,6 @@
 // express or implied.  See the License for the specific language
 // governing permissions and limitations under the License.
 
-// Updated 2015-08-01 by Rei Vilo
-// . Added #include Energia
-// . For Energia, changed pin names to pin numbers (see comment below)
-// . Works on MSP430F5529, LM4F120, TM4C123
-// . Fails on MSP432 and CC3200
-
-// Notice: ***** Generated file: DO _NOT_ MODIFY, Created on: 2016-01-12 00:11:21 UTC *****
-
-
-// Simple demo to toggle EPD between two images.
-
-// Operation from reset:
-// * display version
-// * display compiled-in display setting
-// * display EPD FLASH detected or not
-// * display temperature (displayed before every image is changed)
-// * clear screen
-// * delay 5 seconds (flash LED)
-// * display text image
-// * delay 5 seconds (flash LED)
-// * display picture
-// * delay 5 seconds (flash LED)
-// * back to text display
-
 
 #if defined(ENERGIA)
 #include <Energia.h>
@@ -71,7 +47,6 @@
 // current version number
 #define DEMO_VERSION "5"
 
-
 // pre-processor convert to string
 #define MAKE_STRING1(X) #X
 #define MAKE_STRING(X) MAKE_STRING1(X)
@@ -88,7 +63,6 @@
 #define IMAGE_1_BITS MAKE_NAME(IMAGE_1,EPD_IMAGE_NAME_SUFFIX)
 #define IMAGE_2_FILE MAKE_JOIN(IMAGE_2,EPD_IMAGE_FILE_SUFFIX)
 #define IMAGE_2_BITS MAKE_NAME(IMAGE_2,EPD_IMAGE_NAME_SUFFIX)
-
 
 // Add Images library to compiler path
 #include <Images.h>  // this is just an empty file
@@ -108,121 +82,52 @@ PROGMEM const
 #undef char
 #undef unsigned
 
-
-
-
 // LED anode through resistor to I/O pin
 // LED cathode to Ground
 #define LED_ON  HIGH
 #define LED_OFF LOW
 
-
 // define the E-Ink display
 EPD_Class EPD(EPD_SIZE,
-	      Pin_PANEL_ON,
-	      Pin_BORDER,
-	      Pin_DISCHARGE,
+          Pin_PANEL_ON,
+          Pin_BORDER,
+          Pin_DISCHARGE,
 #if EPD_PWM_REQUIRED
-	      Pin_PWM,
+          Pin_PWM,
 #endif
-	      Pin_RESET,
-	      Pin_BUSY,
-	      Pin_EPD_CS);
+          Pin_RESET,
+          Pin_BUSY,
+          Pin_EPD_CS);
 
-// button setup
-//const int buttonPin = PUSH2;     // the number of the pushbutton pin
-//int buttonState;             // the current reading from the input pin
-//int lastButtonState = LOW;   // the previous reading from the input pin
-//long lastDebounceTime = 0;  // the last time the output pin was toggled
-//long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 
 
 // I/O setup
 void setup() {
-  //__eint();
-  //pinMode(buttonPin, INPUT_PULLUP);
-        
-//        WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-//        P1DIR = 0x01;                             // P1.0 output, else input
-//        P1OUT =  0x8;                            // P1.3 set, else reset
-//        P1REN |= 0x8;                            // P1.3 pullup
-//        P1IE |= 0x8;                             // P1.3 interrupt enabled
-//        P1IES |= 0x8;                            // P1.3 Hi/lo edge
-//        P1IFG &= ~0x8;                           // P1.3 IFG cleared
-        
-	//pinMode(Pin_RED_LED, OUTPUT);
-	//pinMode(Pin_SW2, INPUT);
-	//pinMode(Pin_TEMPERATURE, INPUT);
+
 #if EPD_PWM_REQUIRED
-	pinMode(Pin_PWM, OUTPUT);
+    pinMode(Pin_PWM, OUTPUT);
 #endif
-	pinMode(Pin_BUSY, INPUT);
-	pinMode(Pin_RESET, OUTPUT);
-	pinMode(Pin_PANEL_ON, OUTPUT);
-	pinMode(Pin_DISCHARGE, OUTPUT);
-	pinMode(Pin_BORDER, OUTPUT);
-	pinMode(Pin_EPD_CS, OUTPUT);
-	//pinMode(Pin_EPD_FLASH_CS, OUTPUT);
+    pinMode(Pin_BUSY, INPUT);
+    pinMode(Pin_RESET, OUTPUT);
+    pinMode(Pin_PANEL_ON, OUTPUT);
+    pinMode(Pin_DISCHARGE, OUTPUT);
+    pinMode(Pin_BORDER, OUTPUT);
+    pinMode(Pin_EPD_CS, OUTPUT);
+    //pinMode(Pin_EPD_FLASH_CS, OUTPUT);
 
-	//digitalWrite(Pin_RED_LED, LOW);
 #if EPD_PWM_REQUIRED
-	digitalWrite(Pin_PWM, LOW);
+    digitalWrite(Pin_PWM, LOW);
 #endif
-	digitalWrite(Pin_RESET, LOW);
-	digitalWrite(Pin_PANEL_ON, LOW);
-	digitalWrite(Pin_DISCHARGE, LOW);
-	digitalWrite(Pin_BORDER, LOW);
-	digitalWrite(Pin_EPD_CS, LOW);
-	//digitalWrite(Pin_EPD_FLASH_CS, HIGH);
-
-	//Serial.begin(9600);
-	//delay(500);
-
-//#if defined(__AVR__)
-//	// // indefinite wait for USB CDC serial port to connect.  Arduino Leonardo only
-//	// while (!Serial) {
-//	// }
-//	// additional delay for USB CDC serial port to connect.  Arduino Leonardo only
-//	if (!Serial) {       // allows terminal time to sync as long
-//		delay(500);  // as the serial monitor is opened before
-//	}                    // upload
-//#endif
-
-//	Serial.println();
-//	Serial.println();
-//	Serial.println("Demo version: " DEMO_VERSION);
-//	Serial.println("Display size: " MAKE_STRING(EPD_SIZE));
-//	Serial.println("Film: V" MAKE_STRING(EPD_FILM_VERSION));
-//	Serial.println("COG: G" MAKE_STRING(EPD_CHIP_VERSION));
-//	Serial.println();
-//
-//	Serial.println("Image 1: " IMAGE_1_FILE);
-//	Serial.println("Image 2: " IMAGE_2_FILE);
-//	Serial.println();
-
-//	EPD_FLASH.begin(Pin_EPD_FLASH_CS);
-//	if (EPD_FLASH.available()) {
-//		Serial.println("EPD FLASH chip detected OK");
-//	} else {
-//		uint8_t maufacturer;
-//		uint16_t device;
-//		EPD_FLASH.info(&maufacturer, &device);
-//		Serial.print("unsupported EPD FLASH chip: MFG: 0x");
-//		Serial.print(maufacturer, HEX);
-//		Serial.print("  device: 0x");
-//		Serial.print(device, HEX);
-//		Serial.println();
-//	}
-
-	// configure temperature sensor
-	//S5813A.begin(Pin_TEMPERATURE);
-
-        //__bis_SR_register(LPM3_bits + GIE);       // Enter LPM4 w/interrupt
-        //__bis_SR_register(GIE);       // GIE
-        
-        pinMode(P1_3, INPUT_PULLUP); 
-        attachInterrupt(P1_3, awake, FALLING);
+    digitalWrite(Pin_RESET, LOW);
+    digitalWrite(Pin_PANEL_ON, LOW);
+    digitalWrite(Pin_DISCHARGE, LOW);
+    digitalWrite(Pin_BORDER, LOW);
+    digitalWrite(Pin_EPD_CS, LOW);
+    //digitalWrite(Pin_EPD_FLASH_CS, HIGH);
+      
+    pinMode(P1_3, INPUT_PULLUP); // Attach pullup to 1.3 (button)
+    attachInterrupt(P1_3, awake, FALLING); // Attach falling interrupt to 1.3
 }
 
 
@@ -231,130 +136,59 @@ static int state = 1;
 
 // main loop
 void loop() {
-  changeimage();
-  suspend();
-  //__bis_SR_register(LPM0_bits + GIE);       // Enter LPM4 w/interrupt
-        // read the state of the switch into a local variable:
-//        buttonState = digitalRead(buttonPin);
-//        if (buttonState == 0){
-//          digitalWrite(Pin_RED_LED, LED_ON);
-//          changeimage();
-//          digitalWrite(Pin_RED_LED, LED_OFF);
-//        }
-//        do{
-//                delay(50);
-//                buttonState = digitalRead(buttonPin);
-//                changeimage();
-//                //Serial.println(buttonState);
-////		digitalWrite(Pin_RED_LED, LED_ON);
-////		delay(50);
-////	        digitalWrite(Pin_RED_LED, LED_OFF);
-////		delay(50);
-//        } while (buttonState == 1);
-        
-
-//	// flash LED for 5 seconds
-//	for (int x = 0; x < delay_counts; ++x) {
-//		digitalWrite(Pin_RED_LED, LED_ON);
-//		delay(50);
-//		digitalWrite(Pin_RED_LED, LED_OFF);
-//		delay(50);
-//	}
+  changeimage(); // Change the picture
+  suspend(); // Enter low power mode 4 (0.1uA)
 }
 
+
 void changeimage(){
-  //__eint();
-  //_BIS_SR(GIE);
-  //interrupts();
-	//int temperature = S5813A.read();
-        int temperature = 40;
-//	Serial.print("Temperature = ");
-//	Serial.print(temperature, DEC);
-//	Serial.println(" Celsius");
+    int temperature = 40; // set temp to 40 for faster changing
+    EPD.begin(); // power up the EPD panel
+    EPD.setFactor(temperature); // adjust for current temperature
+    int delay_counts = 0;
+    switch(state) {
+    default:
 
-	EPD.begin(); // power up the EPD panel
-//	switch (EPD.error()) {
-//	case EPD_OK:
-//		Serial.println("EPD: ok");
-//		break;
-//	case EPD_UNSUPPORTED_COG:
-//		Serial.println("EPD: unsuppported COG");
-//		break;
-//	case EPD_PANEL_BROKEN:
-//		Serial.println("EPD: panel broken");
-//		break;
-//	case EPD_DC_FAILED:
-//		Serial.println("EPD: DC failed");
-//		break;
-//	}
-
-	EPD.setFactor(temperature); // adjust for current temperature
-
-	int delay_counts = 0;
-	switch(state) {
-	default:
-//	case 0:         // clear the screen
-//		EPD.clear();
-//		state = 1;
-//		delay_counts = 0;  // reduce delay so first image come up quickly
-//		break;
-
-	case 1:         // clear -> text
+    case 1:         // clear -> text
 #if EPD_IMAGE_ONE_ARG
-		EPD.image(IMAGE_1_BITS);
+        EPD.image(IMAGE_1_BITS);
 #elif EPD_IMAGE_TWO_ARG
-		EPD.image_0(IMAGE_1_BITS);
+        EPD.image_0(IMAGE_1_BITS);
 #else
 #error "unsupported image function"
 #endif
-		++state;
-		break;
+        ++state;
+        break;
 
-	case 2:         // text -> picture
+    case 2:         // text -> picture
 #if EPD_IMAGE_ONE_ARG
-		EPD.image(IMAGE_2_BITS);
+        EPD.image(IMAGE_2_BITS);
 #elif EPD_IMAGE_TWO_ARG
-		EPD.image(IMAGE_1_BITS, IMAGE_2_BITS);
+        EPD.image(IMAGE_1_BITS, IMAGE_2_BITS);
 #else
 #error "unsupported image function"
 #endif
-		++state;
-		break;
+        ++state;
+        break;
 
-	case 3:        // picture -> text
+    case 3:        // picture -> text
 #if EPD_IMAGE_ONE_ARG
-		EPD.image(IMAGE_1_BITS);
+        EPD.image(IMAGE_1_BITS);
 #elif EPD_IMAGE_TWO_ARG
-		EPD.image(IMAGE_2_BITS, IMAGE_1_BITS);
+        EPD.image(IMAGE_2_BITS, IMAGE_1_BITS);
 #else
 #error "unsupported image function"
 #endif
-		state = 2;  // back to picture next time
-		break;
-	}
-	EPD.end();   // power down the EPD panel  
+        state = 2;  // back to picture next time
+        break;
+    }
+    EPD.end();   // power down the EPD panel  
   
 }
 
+// interrupt service routine
 void awake(void)
 {
-  wakeup();
-  //test = 1;
+  wakeup(); // wakeup from lpm4
 }
 
-// Port 1 interrupt service routine
-//#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-//#pragma vector=PORT1_VECTOR
-//__interrupt void Port_1(void)
-//#elif defined(__GNUC__)
-//void __attribute__ ((interrupt(PORT1_VECTOR))) Port_1 (void)
-//#else
-//#error Compiler not supported!
-//#endif
-//{
-//  //P1OUT = 0x01;                            // P1.0 = toggle
-//  changeimage();
-//  P1OUT ^= 0x01;                            // P1.0 = toggle
-//  P1IFG &= ~0x8;                           // P1.3 IFG cleared
-//  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM4 w/interrupt
-//}
